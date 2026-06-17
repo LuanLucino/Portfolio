5e3265490434bcb17c843aa207522dc7ea24dd9d
Luan Lucino <109971413+LuanLucino@users.noreply.github.com>
2026-06-17 10:23:43 -0300

    .

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -146,7 +146,7 @@ window.addEventListener('scroll', () => {
 /* ================================================
    SERVICE WORKER — PWA
    ================================================ */
-if ('serviceWorker' in navigator) {
+if ('serviceWorker' in navigator && location.hostname !== 'localhost' && location.hostname !== '127.0.0.1') {
   window.addEventListener('load', () => {
     navigator.serviceWorker
       .register('service-worker.js')
